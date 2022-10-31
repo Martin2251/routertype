ec940a9342c46b4cebf108f879c7791f6664ef8e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-10-31 07:49:22 +0000

    yoda and luke

--- src/components/Jedi.tsx
+++ src/components/Jedi.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Link, Outlet, Routes, Route} from "react-router-dom"
+import Luke from './Luke'
+import Yoda from './Yoda'
 
 const Jedi = () => {
   return (
-    <div>Jedi</div>
+    <div><p>this is jedi page</p>
+    <Link to="yoda">
+        <p>Yoda</p>
+    </Link>
+    <Link to="luke">
+        <p>Luke</p>
+    </Link>
+    <Routes>
+        <Route path="yoda" element={<Yoda  />} />
+        <Route path="luke" element={<Luke  />} />
+    </Routes>
+    
+    
+    </div>
   )
 }
 
