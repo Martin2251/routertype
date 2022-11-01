ba4908fc56603778381cabae35080a90fc6c68d5
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-01 07:15:55 +0000

    use naviagte and location

--- src/components/Luke.tsx
+++ src/components/Luke.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams,useLocation,useNavigate,Navigate } from 'react-router-dom'
 const Luke = () => {
 
   const params = useParams();
-  console.log(params)
+  const location = useLocation();
+  // keeps track to see what page the user was on previously 
+  const naviagte = useNavigate();
+  // can do to do the redirect
+  console.log(params);
+  console.log(location);
   return (
 
     // instead of url data you can call it whatever you want but it must be the same as on the app page on the :urldata 
