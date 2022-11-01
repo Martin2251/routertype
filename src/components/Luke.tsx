f6973d9f829c0f04730c579f02152ac43cf5ebee
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-01 07:24:26 +0000

    push up

--- src/components/Luke.tsx
+++ src/components/Luke.tsx
@@ -5,14 +5,19 @@ const Luke = () => {
   const params = useParams();
   const location = useLocation();
   // keeps track to see what page the user was on previously 
-  const naviagte = useNavigate();
+  const navigate = useNavigate();
   // can do to do the redirect
+
+
+  const handleClick = () => navigate('empire');
   console.log(params);
   console.log(location);
   return (
 
     // instead of url data you can call it whatever you want but it must be the same as on the app page on the :urldata 
-    <div>This is the Luke {params.urldata} pages</div>
+    <div><p>This is the Luke {params.urldata} pages</p>
+    <button onClick={handleClick}> </button>
+    </div>
   )
 }
 
